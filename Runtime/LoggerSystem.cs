c349f63e575e001fc8433b25a64d0a89b5fe607f
Yuze075 <925581968@qq.com>
2023-03-18 15:50:08 +0800

    Initial commit

---
+++ Runtime/LoggerSystem.cs
@@ -0,0 +1,35 @@
+﻿namespace YuzeToolkit.Framework.LoggerSystem
+{
+    public static class LoggerSystem
+    {
+        public static void Log(string massage)
+        {
+            UnityEngine.Debug.Log(massage);
+        }
+
+        public static void Log(string massage, string[] tags)
+        {
+            UnityEngine.Debug.Log(massage);
+        }
+
+        public static void Warning(string massage)
+        {
+            UnityEngine.Debug.LogWarning(massage);
+        }
+
+        public static void Warning(string massage, string[] tags)
+        {
+            UnityEngine.Debug.LogWarning(massage);
+        }
+
+        public static void Error(string massage)
+        {
+            UnityEngine.Debug.LogError(massage);
+        }
+
+        public static void Error(string massage, string[] tags)
+        {
+            UnityEngine.Debug.LogError(massage);
+        }
+    }
+}
